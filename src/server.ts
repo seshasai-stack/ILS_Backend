b85b76305f78905d0125588470409e8c85b76964
YeshwanthDantuluri98 <yeshwanthdantuluri@gmail.com>
2026-07-15 22:24:54 +0530

    changes to the server.ts

--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,5 @@ import { app } from "./app.js";
 const port = Number(process.env.PORT) || 5000;
 
 app.listen(port, "0.0.0.0", () => {
-  console.log(`Backend running at http://localhost:${port}`);
+  console.log(`Backend running on port ${port}`);
 });
