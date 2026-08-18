f3cbdbdfd405606fefc24e501afa2a1ce48f487e
YeshwanthDantuluri98 <yeshwanthdantuluri@gmail.com>
2026-08-18 23:11:21 +0530

    changes to the price
    changes to the price

--- src/config/pricing.ts
+++ src/config/pricing.ts
@@ -1,16 +1,7 @@
-// export const REGISTRATION_PRICE = {
-//   baseAmount: 39500,
-//   gstRate: 18,
-//   gstAmount: 7110,
-//   totalAmount: 46610,
-//   currency: "INR" as const,
-// };
-
-// Test
 export const REGISTRATION_PRICE = {
-  baseAmount: 10,
-  gstRate: 0,
-  gstAmount: 0,
-  totalAmount: 10,
+  baseAmount: 39500,
+  gstRate: 18,
+  gstAmount: 7110,
+  totalAmount: 46610,
   currency: "INR" as const,
 };
