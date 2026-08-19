0ec0c191e2a68e7504cce19831e5ce3337dfc4d0
YeshwanthDantuluri98 <yeshwanthdantuluri@gmail.com>
2026-08-19 15:38:13 +0530

    test

--- src/config/pricing.ts
+++ src/config/pricing.ts
@@ -1,7 +1,15 @@
+// export const REGISTRATION_PRICE = {
+//   baseAmount: 39500,
+//   gstRate: 18,
+//   gstAmount: 7110,
+//   totalAmount: 46610,
+//   currency: "INR" as const,
+// };
+//test
 export const REGISTRATION_PRICE = {
-  baseAmount: 39500,
-  gstRate: 18,
-  gstAmount: 7110,
-  totalAmount: 46610,
+  baseAmount: 10,
+  gstRate: 0,
+  gstAmount: 0,
+  totalAmount: 10,
   currency: "INR" as const,
 };
